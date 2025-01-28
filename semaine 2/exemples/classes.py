1dbad599a04c5cf590571787209abd5a05f7ba6b
Pier Luc Ducharme <dutch80@gmail.com>
2025-01-28 16:00:46 -0500

    modifications exemples semaine 2

--- semaine 2/exemples/classes.py
+++ semaine 2/exemples/classes.py
@@ -1,5 +1,6 @@
-# Définition d'une classe
+# Définition d'une classe de base sans héritage
 # class <NomDeLaClasse>:
+# Si aucune classe parente n'est spécifiée, la classe hérite de la classe object
 class Parent:
 
     def __init__(self, nom, prenom):
