501db9560828a488bc9f875367cc9986680e7854
Pier Luc Ducharme <dutch80@gmail.com>
2025-02-03 20:11:27 -0500

    ajout semaine 3

---
+++ Semaine 3/exemples/examplepackage/example.py
@@ -0,0 +1,17 @@
+import math
+
+class UneClasse:
+
+    def methode(self):
+        pass
+
+class UneAutreClasse:
+
+    def valeur_de_pi(self):
+        return math.pi
+
+
+print(__name__)
+
+if __name__ == "__main__":
+    print("Sera seulement exécuté si le module est exécuté directement")
